c601a5bb80a5d2c966327879980c03bd5c828887
Zane Dunnings <zane@Zanes-MacBook-Pro.local>
2019-01-17 12:25:24 -0500

    added route for setting criteria

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ def run_query(query): # A simple function to use requests.post to make the API c
 
 query = """
 {
-    search(term: "coffee",
+    search(term: "%s",
             location: "%s",
             limit: 10,
-            radius: 500) {
+            radius: 300) {
         total
         business {
             name
-            url
+            rating
         }
     }
 }
@@ -40,17 +40,36 @@ query = """
 
 @app.route('/nearby', methods=['POST'])
 def getNearby():
+    """
+    returns the top ten locations within 300 meters of the given address that
+    fit the term type
+    """
     if request.method == 'POST':
         data = request.args.to_dict()
-        print(data)
-        print("hi")
         address = data['address']
-        queryWithAddress = query % address
+        term = data['term']
+        queryWithAddress = query % (term, address)
         print(queryWithAddress)
         result = run_query(queryWithAddress) # Execute the query
         return json.dumps(result, indent=4, sort_keys=True)
     else:
         return ""
+
+@app.route('/criteria', methods=['PUT'])
+def setCriteria():
+    """
+    sets particular criteria to a score between 0 and 3
+    """
+    if request.method == 'PUT':
+        data = request.args.to_dict()
+        criteria = data['criteria']
+        value = data['value']
+        hash = firebase.get('/criteria/', None)
+        result = firebase.put('/criteria/',criteria, {criteria: value})
+        return json.dumps(result, indent=4, sort_keys=True)
+    else:
+        return "didnt go through"
+
 
 
 if __name__ == '__main__':
